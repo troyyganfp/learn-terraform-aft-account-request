2cd57965b7f393ee835754988573d4460e63c96b
troyyganfp <160545456+troyyganfp@users.noreply.github.com>
2024-08-12 09:16:50 +1200

    added sandbox-aft-2 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,26 @@ module "sandbox" {
   }
   account_customizations_name = "sandbox-aft"
 }
+
+module "sandbox-2" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "aws.aft-testing-2@fisherpaykel.com"
+    AccountName               = "Sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws.aft-testing-2@fisherpaykel.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+  custom_fields = {
+    group = "sg_aws_devops"
+  }
+  account_customizations_name = "sandbox-aft-2"
+}
