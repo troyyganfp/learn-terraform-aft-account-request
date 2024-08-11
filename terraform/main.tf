b32402e92d41be453a7b85978d75d53300cef587
troyyganfp <160545456+troyyganfp@users.noreply.github.com>
2024-08-11 19:56:47 +1200

    test create sandox-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,22 @@
 module "sandbox" {
   source = "./modules/aft-account-request"
-
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "aws.aft-testing"
+    AccountName               = "Sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws.aft-testing"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
-
   account_tags = {
     "Learn Tutorial" = "AFT"
   }
-
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
-
   custom_fields = {
-    group = "non-prod"
+    group = "sg_aws_devops"
   }
-
-  account_customizations_name = "sandbox"
+  account_customizations_name = "sandbox-aft"
 }
